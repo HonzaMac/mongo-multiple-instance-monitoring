a485646dc765929705a56b2707f6b14dc9ce3253
Honza Machala <jan.machala@bileto.cz>
2016-05-11 22:34:03 +0200

    ServesStats, DbStats, all via non blocking react-mongodb library

--- src/WebSocket/MongoApplication.php
+++ src/WebSocket/MongoApplication.php
@@ -16,7 +16,6 @@ use Jmikola\React\MongoDB\ConnectionFactory;
 use Jmikola\React\MongoDB\Protocol\Query;
 use Jmikola\React\MongoDB\Protocol\Reply;
 use MongoMonitoring\Websocket\Messages;
-use MongoMonitoring\WebSocket\Messages\Init;
 use React\Promise\Deferred;
 use React\Promise\FulfilledPromise;
 use React\Promise\Promise;
@@ -25,12 +24,6 @@ use React\SocketClient\ConnectionException;
 
 class MongoApplication implements Application
 {
-    const DELAY = 0.01; // 100ms
-    const DELAY_SERVER_STATUS = 0.5; // 100ms
-
-    /** @var array */
-    private $instanceList;
-
     /**
      * @var array
      */
@@ -90,11 +83,10 @@ class MongoApplication implements Application
         $connection = (yield Awaitable\adapt($this->factory->create($host, $port, ['connectTimeoutMS' => 500, 'socketTimeoutMS' => 500])));
 
         $listDatabasesQuery = new Query('admin.$cmd', ['listDatabases' => 1], null, 0 ,1);
-
         /** @var Reply $reply */
         $reply = (yield Awaitable\adapt($connection->send($listDatabasesQuery)));
         $listDbs = current(iterator_to_array($reply->getIterator()));
-        $initResponse = Init::create($instanceIp, $listDbs);
+        $initResponse = Messages\Init::create($instanceIp, $listDbs);
 
         yield $websocketConnection->send($initResponse);
 
@@ -104,52 +96,19 @@ class MongoApplication implements Application
         $response = Messages\ServerStatus::create($initResponse->getHostId(), $serverStatus);
         yield $websocketConnection->send($response);
 
-        $dbStatsQuery = new Query('admin.$cmd', ['dbStats' => 1], null, 0, 1); // this works
-        $reply = (yield Awaitable\adapt($connection->send($dbStatsQuery)));
-        $dbStats = current(iterator_to_array($reply->getIterator()));
-        $response = Messages\DbStats::create($initResponse->getHostId(), $dbStats);
-        yield $websocketConnection->send($response);
-
-
-        yield $connection->close();
-
-//        $this->fetchDatabaseInfo($listDbs, $connection);
-    }
-
-    /**
-     * @param $listDbs
-     * @param $connection
-     */
-    private function fetchDatabaseInfo($listDbs, $connection)
-    {
         $databases = $listDbs['databases'];
         foreach ($databases as $dbId => $db) {
             $dbName = $db['name'];
-//            $connection = (yield Awaitable\adapt($this->factory->create($host, $port, ['connectTimeoutMS' => 500, 'socketTimeoutMS' => 500])));
+            $dbStatsQuery = new Query($dbName . '.$cmd', ['dbStats' => 1], null, 0, 1);
+            $reply = (yield Awaitable\adapt($connection->send($dbStatsQuery)));
+            $dbStats = current(iterator_to_array($reply->getIterator()));
+            $response = Messages\DbStats::create($initResponse->getHostId(), $dbStats);
+            yield $websocketConnection->send($response);
 
-
-            $selectDbQuery = new Query('admin.$cmd', ['db' => $dbName]);
-            $reply = (yield Awaitable\adapt($connection->send($selectDbQuery)));
-            var_dump($reply);
-            $dbSelect = current(iterator_to_array($reply->getIterator()));
-            var_dump($dbSelect);
-
-//            $selectDbQuery = new Query('admin.$cmd', ['whatsmyuri'=> 1], null, 0, 1);
-//            $reply = (yield Awaitable\adapt($connection->send($selectDbQuery)));
-//            $dbSelect = current(iterator_to_array($reply->getIterator()));
-//            var_dump($dbSelect);
-//
-//            $dbStatusQuery = new Query($dbName, ['dbStats' => 1], null, 0, 1); // this works
-//            var_dump($dbStatusQuery);
-//            $reply = (yield Awaitable\adapt($connection->send($dbStatusQuery)));
-//            $dbStatus = current(iterator_to_array($reply->getIterator()));
-//            var_dump($dbStatus);
-//            $response = DbStatus::create($initResponse->getHostId(), $dbName, $dbStatus);
-//            yield $websocketConnection->send($response);
         }
+
+        yield $connection->close();
     }
-
-
 }
 
 //  $db['stats'] = \MongoMonitoring\command($mongoDb, 'db.stats()');
