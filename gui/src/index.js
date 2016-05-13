7264613c08f399654c09b54e41595ecdb4c431bb
Honza Machala <honza.machala@seznam.cz>
2016-05-13 13:12:44 +0200

    Match host & port params from url and set it as websocketUrl

--- gui/src/index.js
+++ gui/src/index.js
@@ -10,11 +10,18 @@ if (! "WebSocket" in window) {
 	alert("WebSocket NOT supported by your Browser!");
 
 } else if(document.getElementById('app')) {
+
+	const query = window.location.search;
+	const host = /host=([a-z0-9-_\.@:]+)/ig.exec(query);
+	const port = /port=([\d]+)/ig.exec(query);
+
+	const url = host && port ? host[1] + ':' + port[1] : 'localhost:9900';
+
 	/**
 	 * Render JS application into html element
 	 */
 	render(
-		<App websocketUrl="localhost:9900" />,
+		<App websocketUrl={url} />,
 		document.getElementById('app')
 	);
 }
