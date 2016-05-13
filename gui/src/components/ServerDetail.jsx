e07e2d679730e06382019ce65272cf40efb33c40
Honza Machala <honza.machala@seznam.cz>
2016-05-13 14:34:40 +0200

    Show database statistics

--- gui/src/components/ServerDetail.jsx
+++ gui/src/components/ServerDetail.jsx
@@ -6,7 +6,7 @@ import JsonDetail from './JsonDetail.jsx';
 export default class ServerDetail extends React.Component {
 
     render() {
-        const {hostInfo, buildInfo, init, index, log} = this.props;
+        const {hostInfo, buildInfo, init, index, log, dbStats} = this.props;
 
         return (
             <div className={Classname('col-md-6 col-xs-12', {clearfix: index % 2 === 0})}>
@@ -26,14 +26,14 @@ export default class ServerDetail extends React.Component {
                     <div id={`panel-collapse-${index}`} className="panel-collapse collapse in" aria-expanded={index === 0}>
                         <div className="panel-body">
 
-                            <h4>Host info <JsonDetail title="Host info" code={JSON.stringify(hostInfo.length && hostInfo[0].data)} /></h4>
-                            {this.renderHostInfo(hostInfo.length && hostInfo[0].data)}
+                            <h4>Host info <JsonDetail title="Host info" code={JSON.stringify(hostInfo.length && hostInfo[hostInfo.length - 1].data)} /></h4>
+                            {this.renderHostInfo(hostInfo.length && hostInfo[hostInfo.length - 1].data)}
 
-                            <h4>Build info <JsonDetail title="Build info" code={JSON.stringify(buildInfo.length && buildInfo[0].data)} /></h4>
-                            {this.renderBuildInfo(buildInfo.length && buildInfo[0].data)}
+                            <h4>Build info <JsonDetail title="Build info" code={JSON.stringify(buildInfo.length && buildInfo[buildInfo.length - 1].data)} /></h4>
+                            {this.renderBuildInfo(buildInfo.length && buildInfo[buildInfo.length - 1].data)}
 
-                            <h4>Databases <JsonDetail title="Databases" code={JSON.stringify(init.length && init[0].listDBs.databases)} /></h4>
-                            {this.renderDatabases(init.length && init[0].listDBs.databases)}
+                            <h4>Databases <JsonDetail title="Databases" code={JSON.stringify(init.length && init[init.length - 1].listDBs.databases)} /></h4>
+                            {this.renderDatabases(init.length && init[init.length - 1].listDBs.databases, dbStats.length && dbStats[dbStats.length - 1].data)}
 
                             <h4>Logs</h4>
                             {this.renderLogMessages(log && log.length && log[log.length - 1].data)}
@@ -81,13 +81,26 @@ export default class ServerDetail extends React.Component {
         )
     }
 
-    renderDatabases(data) {
+    renderDatabases(data, stats) {
+
+        const renderStats = (stats) => {
+          return (
+              <p>
+                  Collections <span className="badge">{stats.collections}</span><br />
+                  Indexes <span className="badge">{stats.indexes}</span><br />
+                  Index sizes <span className="badge">{stats.indexSize}</span><br />
+                  File size <span className="badge">{stats.fileSize}</span>
+              </p>
+          )
+        };
+
         return (
             <ul className="list-group">
                 {data.sort((a, b) => a.name > b.name).map((db) => {
                     return (
                         <li className="list-group-item">
-                            {db.name}
+                            <strong>{db.name}</strong>
+                            {stats && stats.db == db.name ? renderStats(stats) : null}
                         </li>
                     )
                 })}
