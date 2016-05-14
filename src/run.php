69f3c1a7e0315e2db1ff35d37210252017e15341
Honza Machala <jan.machala@bileto.cz>
2016-05-14 21:44:24 +0200

    Set specific localhost and port

--- src/run.php
+++ src/run.php
@@ -13,14 +13,14 @@ ini_set('xdebug.max_nesting_level', PHP_INT_MAX);
 ini_set('mongo.long_as_object', 1);
 
 
-$defaultPort = 9900;
 $configPath = 'config.neon';
 $fileContent = loadFile($configPath);
 if (null === $fileContent) {
     $fileContent = loadFile('../' . $configPath);
 }
 $config = Neon::decode($fileContent);
-$port = isset($config['server']['port']) ? $config['server']['port'] : $defaultPort;
+$port = isset($config['server']['port']) ? $config['server']['port'] : 9900;
+$host = isset($config['server']['host']) ? $config['server']['host'] : '0.0.0.0';
 $explodedHosts = explode(',', getenv('HOSTS'));
 $envHosts = (isset($explodedHosts[0]) && $explodedHosts[0] === '') ? [] : $explodedHosts;
 $config['hosts'] = array_merge(
@@ -29,7 +29,7 @@ $config['hosts'] = array_merge(
 
 $loop = Loop\loop();
 $server = new Server(new Handler(new ReactLoop($loop), $config['hosts']));
-$server->listen($port);
+$server->listen($port, '0.0.0.0');
 echo 'Websocket server is listening on port ' . $port . PHP_EOL;
 
 $loop->run();
